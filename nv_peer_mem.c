b3ce868b70ba0537a84b660502bac069a3b3f0d7
Davide Rossetti <davide.rossetti@gmail.com>
2018-10-24 15:58:35 -0700

    fortify context management against MOFED 4.0

--- nv_peer_mem.c
+++ nv_peer_mem.c
@@ -50,7 +50,9 @@
 #define DRV_VERSION	"1.0-8"
 #define DRV_RELDATE	__DATE__
 
-#define peer_err(FMT, ARGS...) printk(KERN_ERR   DRV_NAME " %s:%d " FMT, __FUNCTION__, __LINE__, ## ARGS)
+#define peer_err(FMT, ARGS...)  printk(KERN_ERR   DRV_NAME "ERR %s:%d " FMT, __FUNCTION__, __LINE__, ## ARGS)
+#define peer_info(FMT, ARGS...) printk(KERN_ERR   DRV_NAME "INFO %s:%d " FMT, __FUNCTION__, __LINE__, ## ARGS)
+#define peer_dbg(FMT, ARGS...)  printk(KERN_ERR   DRV_NAME "DBG %s:%d " FMT, __FUNCTION__, __LINE__, ## ARGS)
 
 #ifndef NVIDIA_P2P_MAJOR_VERSION_MASK
 #define NVIDIA_P2P_MAJOR_VERSION_MASK   0xffff0000
@@ -106,6 +108,7 @@ invalidate_peer_memory mem_invalidate_callback;
 static void *reg_handle;
 
 struct nv_mem_context {
+	uint64_t guard0;
 	struct nvidia_p2p_page_table *page_table;
 #if NV_DMA_MAPPING
 	struct nvidia_p2p_dma_mapping *dma_mapping;
@@ -122,8 +125,110 @@ struct nv_mem_context {
 	unsigned long page_size;
 	int is_callback;
 	int sg_allocated;
+	struct list_head node;
+	uint64_t guard1;
 };
 
+struct nv_ctx_list {
+	struct list_head head;
+	spinlock_t lock;
+} ctx_list;
+
+static int __ctxlist_is_tracked(struct nv_mem_context *ctx)
+{
+	int rc = 0;
+	struct list_head *cur = NULL;
+	
+	list_for_each(cur, &ctx_list.head) {
+		struct nv_mem_context *cur_ctx = list_entry(cur, struct nv_mem_context, node);
+		if (cur_ctx == ctx) {
+			rc = 1;
+			break;
+		}
+	}
+	return rc;
+}
+
+static int ctxlist_is_tracked(struct nv_mem_context *ctx)
+{
+	int rc = 0;
+	unsigned long flags;
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	rc = __ctxlist_is_tracked(ctx);
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
+	return rc;
+}
+
+static int __ctxlist_add(struct nv_mem_context *ctx)
+{
+	int rc = 0;
+	// check for dups
+	if (!ctx) {
+		peer_err("invalid NULL ctx\n");
+		rc = EINVAL;
+		goto out;
+	}
+	if (__ctxlist_is_tracked(ctx)) {
+		peer_err("ouch, ignoring dup entry for ctx=%px, will not add new instance!!!\n", ctx);
+		rc = EAGAIN;
+		goto out;
+	}
+	list_add_tail(&ctx->node, &ctx_list.head);
+ out:
+	return rc;
+}
+
+static int ctxlist_add(struct nv_mem_context *ctx)
+{
+	int rc = 0;
+	unsigned long flags;
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	rc = __ctxlist_add(ctx);
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
+	return rc;
+}
+
+static int __ctxlist_del(struct nv_mem_context *ctx)
+{
+	int rc = 0;
+	// check for dups
+	if (!__ctxlist_is_tracked(ctx)) {
+		peer_err("ouch, ctx=%px is not tracked, while trying to remove from list, nothing to do\n", ctx);
+		rc = EINVAL;
+		goto out;
+	}
+	list_del(&ctx->node);
+ out:
+	return rc;
+}
+
+static int ctxlist_del(struct nv_mem_context *ctx)
+{
+	int rc = 0;
+	unsigned long flags;
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	rc = __ctxlist_del(ctx);
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
+	return rc;
+}
+
+static int ctxlist_is_empty(void)
+{
+	int rc = 0;
+	unsigned long flags;
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	if (list_empty(&ctx_list.head)) {
+		rc = 1;
+	}
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
+	return rc;
+}
+
+static void ctxlist_init(void)
+{
+	INIT_LIST_HEAD(&ctx_list.head);
+	spin_lock_init(&ctx_list.lock);
+}
 
 static void nv_get_p2p_free_callback(void *data)
 {
@@ -133,15 +238,23 @@ static void nv_get_p2p_free_callback(void *data)
 #if NV_DMA_MAPPING
 	struct nvidia_p2p_dma_mapping *dma_mapping = NULL;
 #endif
-
+	unsigned long flags;
+	
 	__module_get(THIS_MODULE);
+	spin_lock_irqsave(&ctx_list.lock, flags);
+
 	if (!nv_mem_context) {
-		peer_err("nv_get_p2p_free_callback -- invalid nv_mem_context\n");
+		peer_err("invalid nv_mem_context\n");
+		goto out;
+	}
+
+	if (!__ctxlist_is_tracked(nv_mem_context)) {
+		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
 		goto out;
 	}
 
 	if (!nv_mem_context->page_table) {
-		peer_err("nv_get_p2p_free_callback -- invalid page_table\n");
+		peer_err("invalid page_table\n");
 		goto out;
 	}
 
@@ -161,19 +274,36 @@ static void nv_get_p2p_free_callback(void *data)
 	/* For now don't set nv_mem_context->page_table to NULL, 
 	  * confirmed by NVIDIA that inflight put_pages with valid pointer will fail gracefully.
 	*/
-
 	WRITE_ONCE(nv_mem_context->is_callback, 1);
-	(*mem_invalidate_callback) (reg_handle, nv_mem_context->core_context);
+	wmb();
+
+	peer_err("nv_mem_context:%px page_table:%px dma_mapping:%px VA:%llx-%llx npages:%lu\n",
+		 nv_mem_context, page_table, dma_mapping, nv_mem_context->page_virt_start, nv_mem_context->page_virt_end, nv_mem_context->npages);
+
+	// holding ctx_list lock
+	(*mem_invalidate_callback) (reg_handle, (uint64_t)nv_mem_context->core_context);
+
 #if NV_DMA_MAPPING
-	ret = nvidia_p2p_free_dma_mapping(dma_mapping); 
-	if (ret)
-                peer_err("nv_get_p2p_free_callback -- error %d while calling nvidia_p2p_free_dma_mapping()\n", ret);
+	if (!dma_mapping) {
+		peer_err("invalid dma_mapping\n");
+	} else {
+		ret = nvidia_p2p_free_dma_mapping(dma_mapping);
+		if (ret)
+			peer_err("nv_get_p2p_free_callback -- error %d while calling nvidia_p2p_free_page_table()\n", ret);
+		nv_mem_context->dma_mapping = NULL;
+	}
 #endif
-	ret = nvidia_p2p_free_page_table(page_table);
-	if (ret)
-		peer_err("nv_get_p2p_free_callback -- error %d while calling nvidia_p2p_free_page_table()\n", ret);
+	if (!page_table) {
+		peer_err("invalid page_table\n");
+	} else {
+		ret = nvidia_p2p_free_page_table(page_table);
+		if (ret)
+			peer_err("nv_get_p2p_free_callback -- error %d while calling nvidia_p2p_free_page_table()\n", ret);
+		nv_mem_context->page_table = NULL;
+	}
 
 out:
+	spin_unlock_irqrestore(&ctx_list.lock, flags);	
 	module_put(THIS_MODULE);
 	return;
 
@@ -189,7 +319,7 @@ static void nv_mem_dummy_callback(void *data)
 
 	ret = nvidia_p2p_free_page_table(nv_mem_context->page_table);
 	if (ret)
-		peer_err("nv_mem_dummy_callback -- error %d while calling nvidia_p2p_free_page_table()\n", ret);
+		peer_err("nv_mem_dummy_callback --  error %d while calling nvidia_p2p_free_page_table()\n", ret);
 
 	module_put(THIS_MODULE);
 	return;
@@ -230,6 +360,12 @@ static int nv_mem_acquire(unsigned long addr, size_t size, void *peer_mem_privat
 
 	/* 1 means mine */
 	*client_context = nv_mem_context;
+
+	if (ctxlist_add(nv_mem_context)) {
+		peer_err("error, failing acquire for dup context %px\n", nv_mem_context);
+		goto err;
+	}
+	
 	__module_get(THIS_MODULE);
 	return 1;
 
@@ -248,7 +384,19 @@ static int nv_dma_map(struct sg_table *sg_head, void *context,
 	struct scatterlist *sg;
 	struct nv_mem_context *nv_mem_context =
 		(struct nv_mem_context *) context;
-	struct nvidia_p2p_page_table *page_table = nv_mem_context->page_table;
+	struct nvidia_p2p_page_table *page_table;
+
+	if (!ctxlist_is_tracked(nv_mem_context)) {
+		peer_err("error, invalid ctx %px\n", nv_mem_context);
+            return -EINVAL;
+	}
+
+	page_table = nv_mem_context->page_table;
+
+        if (!page_table) {
+            peer_err("error, invalid p2p page table\n");
+            return -EINVAL;
+        }
 
 	if (page_table->page_size != NVIDIA_P2P_PAGE_SIZE_64KB) {
 		peer_err("nv_dma_map -- assumption of 64KB pages failed size_id=%u\n",
@@ -327,28 +475,49 @@ static int nv_dma_map(struct sg_table *sg_head, void *context,
 static int nv_dma_unmap(struct sg_table *sg_head, void *context,
 			   struct device  *dma_device)
 {
+	int ret = 0;
 	struct nv_mem_context *nv_mem_context =
 		(struct nv_mem_context *)context;
-
+	unsigned long flags;
+	
 	if (!nv_mem_context) {
 		peer_err("nv_dma_unmap -- invalid nv_mem_context\n");
 		return -EINVAL;
 	}
 
-	if (READ_ONCE(nv_mem_context->is_callback))
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	if (!__ctxlist_is_tracked(nv_mem_context)) {
+		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
+		ret = -EAGAIN;
 		goto out;
+	}
+
+	peer_dbg("nv_mem_context:%px page_table:%px dma_mapping:%px is_callback:%d\n", nv_mem_context, nv_mem_context->page_table, nv_mem_context->dma_mapping, READ_ONCE(nv_mem_context->is_callback));
+	
+	if (READ_ONCE(nv_mem_context->is_callback)) {
+		// do nothing
+		ret = 0;
+		goto out;
+	}
+
+	if (!nv_mem_context->sg_allocated) {
+		peer_err("error, sg is not allocated\n");
+		ret = -EINVAL;
+		goto out;
+	}
 
 #if NV_DMA_MAPPING
-	{
+	if (nv_mem_context->dma_mapping) {
 		struct pci_dev *pdev = to_pci_dev(dma_device);
-		if (nv_mem_context->dma_mapping)
-			nvidia_p2p_dma_unmap_pages(pdev, nv_mem_context->page_table,
-						   nv_mem_context->dma_mapping);
+		//peer_dbg("freeing dma_mapping %px\n", nv_mem_context->dma_mapping);
+		nvidia_p2p_dma_unmap_pages(pdev, nv_mem_context->page_table, nv_mem_context->dma_mapping);
+		nv_mem_context->dma_mapping = NULL;
 	}
 #endif
 
 out:
-	return 0;
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
+	return ret;
 }
 
 
@@ -357,6 +526,18 @@ static void nv_mem_put_pages(struct sg_table *sg_head, void *context)
 	int ret = 0;
 	struct nv_mem_context *nv_mem_context =
 		(struct nv_mem_context *) context;
+	unsigned long flags;
+
+	if (!nv_mem_context) {
+		peer_err("invalid context %px\n", nv_mem_context);
+		return;
+	}
+
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	if (!__ctxlist_is_tracked(nv_mem_context)) {
+		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
+		goto out;
+	}
 
 	if (READ_ONCE(nv_mem_context->is_callback))
 		goto out;
@@ -381,6 +562,7 @@ out:
 		nv_mem_context->sg_allocated = 0;
 	}
 
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
 	return;
 }
 
@@ -388,7 +570,7 @@ static void nv_mem_release(void *context)
 {
 	struct nv_mem_context *nv_mem_context =
 		(struct nv_mem_context *) context;
-
+	ctxlist_del(nv_mem_context);
 	kfree(nv_mem_context);
 	module_put(THIS_MODULE);
 	return;
@@ -404,28 +586,40 @@ static int nv_mem_get_pages(unsigned long addr,
 			  u64 core_context)
 #endif
 {
-	int ret;
-	struct nv_mem_context *nv_mem_context;
+	int ret = 0;
+	struct nv_mem_context *nv_mem_context = (struct nv_mem_context *)client_context;
+	unsigned long flags;
 
-	nv_mem_context = (struct nv_mem_context *)client_context;
-	if (!nv_mem_context)
+	if (!nv_mem_context) {
+		peer_err("invalid context\n");
 		return -EINVAL;
+	}
 
-	nv_mem_context->core_context = core_context;
+	spin_lock_irqsave(&ctx_list.lock, flags);
+	if (!__ctxlist_is_tracked(nv_mem_context)) {
+		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
+		ret = -EINVAL;
+		goto out;
+	}
+	
+        peer_dbg("addr=%lx size=%zu\n", addr, size);
+
+	nv_mem_context->core_context = (void *)core_context;
 	nv_mem_context->page_size = GPU_PAGE_SIZE;
 
+	// deadlock if call below were to generate a callback
 	ret = nvidia_p2p_get_pages(0, 0, nv_mem_context->page_virt_start, nv_mem_context->mapped_size,
 			&nv_mem_context->page_table, nv_get_p2p_free_callback, nv_mem_context);
 	if (ret < 0) {
 		peer_err("nv_mem_get_pages -- error %d while calling nvidia_p2p_get_pages()\n", ret);
-		return ret;
 	}
-
 	/* No extra access to nv_mem_context->page_table here as we are
 	    called not under a lock and may race with inflight invalidate callback on that buffer.
 	    Extra handling was delayed to be done under nv_dma_map.
 	 */
-	return 0;
+ out:
+	spin_unlock_irqrestore(&ctx_list.lock, flags);
+	return ret;
 }
 
 
@@ -458,12 +652,18 @@ static int __init nv_mem_client_init(void)
 	if (!reg_handle)
 		return -EINVAL;
 
+	ctxlist_init();
+	
 	return 0;
 }
 
 static void __exit nv_mem_client_cleanup(void)
 {
+        peer_info("unloading %s:%s\n", DRV_NAME, DRV_VERSION);
 	ib_unregister_peer_memory_client(reg_handle);
+	if (!ctxlist_is_empty()) {
+		peer_err("error, ctx list not empty\n");
+	}
 }
 
 module_init(nv_mem_client_init);
