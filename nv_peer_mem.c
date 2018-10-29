9354b8569848e833aab70fc11eb2b2d78d3c5c95
Davide Rossetti <davide.rossetti@gmail.com>
2018-10-29 14:26:26 -0700

    narrow locking down to dma unmapping/freeing calls, leave ctx list as debugging aid

--- nv_peer_mem.c
+++ nv_peer_mem.c
@@ -147,29 +147,21 @@ struct nv_mem_context {
 };
 
 
-#if 0
-typedef spinlock_t ctxlist_lock_t;
-#define ctxlist_lock_init()				\
-	spin_lock_init(&ctx_list.lock)
-#define ctxlist_lock()					\
-	unsigned long flags;				\
-	spin_lock_irqsave(&ctx_list.lock, flags);
-#define cxtlist_unlock()				\
-	spin_unlock_irqrestore(&ctx_list.lock, flags)
-#else
 typedef struct mutex ctxlist_lock_t;
+
 #define ctxlist_lock_init()				\
-	mutex_init(&ctx_list.lock)
+        mutex_init(&ctx_list.lock)
 #define ctxlist_lock()					\
 	mutex_lock(&ctx_list.lock)
 #define ctxlist_unlock()				\
 	mutex_unlock(&ctx_list.lock)
-#endif
 
 struct nv_ctx_list {
 	struct list_head head;
 	ctxlist_lock_t lock;
 } ctx_list;
+
+DEFINE_MUTEX(api_lock);
 
 
 static int __ctxlist_is_tracked(struct nv_mem_context *ctx)
@@ -288,7 +280,7 @@ static void nv_get_p2p_free_callback(void *data)
 		goto out;
 	}
 
-	if (!__ctxlist_is_tracked(nv_mem_context)) {
+	if (!ctxlist_is_tracked(nv_mem_context)) {
 		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
 		goto out;
 	}
@@ -329,7 +321,9 @@ static void nv_get_p2p_free_callback(void *data)
 		peer_err("invalid dma_mapping\n");
 	} else {
 		peer_dbg("calling nv_free_dma_mapping\n");
+		mutex_lock(&api_lock);
 		ret = nvidia_p2p_free_dma_mapping(dma_mapping);
+		mutex_unlock(&api_lock);
 		if (ret)
 			peer_err("nv_get_p2p_free_callback -- error %d while calling nvidia_p2p_free_page_table()\n", ret);
 		nv_mem_context->dma_mapping = NULL;
@@ -348,7 +342,7 @@ static void nv_get_p2p_free_callback(void *data)
 	peer_dbg("before has pending\n");
 	if (READ_ONCE(nv_mem_context->has_pending_release)) {
 		peer_dbg("before list del\n");
-		if (__ctxlist_del(nv_mem_context)) {
+		if (ctxlist_del(nv_mem_context)) {
 			peer_err("error, while dequeuing nv_mem_context:%px\n", nv_mem_context);
 		}
 		peer_dbg("before kfree\n");
@@ -357,7 +351,6 @@ static void nv_get_p2p_free_callback(void *data)
 	}
 out:
 	peer_dbg("before unlock\n");
-	ctxlist_unlock();	
 	module_put(THIS_MODULE);
 	peer_dbg("invalidation completed\n");
 	return;
@@ -443,8 +436,7 @@ static int nv_dma_map(struct sg_table *sg_head, void *context,
 		(struct nv_mem_context *) context;
 	struct nvidia_p2p_page_table *page_table;
 
-	ctxlist_lock();
-	if (!__ctxlist_is_tracked(nv_mem_context)) {
+	if (!ctxlist_is_tracked(nv_mem_context)) {
 		peer_err("error, invalid ctx %px\n", nv_mem_context);
 		ret = -EINVAL;
 		goto out;
@@ -543,7 +535,6 @@ static int nv_dma_map(struct sg_table *sg_head, void *context,
 
 	*nmap = nv_mem_context->npages;
  out:
-	ctxlist_unlock();
 	return ret;
 }
 
@@ -559,19 +550,16 @@ static int nv_dma_unmap(struct sg_table *sg_head, void *context,
 		return -EINVAL;
 	}
 
-	// unprotected access to the context, we would need a recursive lock otherwise
-	if (READ_ONCE(nv_mem_context->is_callback)) {
-		peer_dbg("early exit in a callback\n");
-		return 0;
-	}
-
-	peer_dbg("before lock\n");
-	ctxlist_lock();
 	peer_dbg("before is tracked\n");
-	if (!__ctxlist_is_tracked(nv_mem_context)) {
+	if (!ctxlist_is_tracked(nv_mem_context)) {
 		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
 		ret = -EAGAIN;
 		goto out;
+	}
+
+	if (READ_ONCE(nv_mem_context->is_callback)) {
+		peer_dbg("early exit in a callback\n");
+		return 0;
 	}
 
 	peer_dbg("nv_mem_context:%px page_table:%px dma_mapping:%px is_callback:%d\n", nv_mem_context, nv_mem_context->page_table, nv_mem_context->dma_mapping, READ_ONCE(nv_mem_context->is_callback));
@@ -586,13 +574,14 @@ static int nv_dma_unmap(struct sg_table *sg_head, void *context,
 	if (nv_mem_context->dma_mapping) {
 		struct pci_dev *pdev = to_pci_dev(dma_device);
 		//peer_dbg("freeing dma_mapping %px\n", nv_mem_context->dma_mapping);
+		mutex_lock(&api_lock);
 		nvidia_p2p_dma_unmap_pages(pdev, nv_mem_context->page_table, nv_mem_context->dma_mapping);
+		mutex_unlock(&api_lock);
 		nv_mem_context->dma_mapping = NULL;
 	}
 #endif
 
 out:
-	ctxlist_unlock();
 	return ret;
 }
 
@@ -609,20 +598,17 @@ static void nv_mem_put_pages(struct sg_table *sg_head, void *context)
 		return;
 	}
 
-	// unprotected access to the context, we would need a recursive lock otherwise
-	if (READ_ONCE(nv_mem_context->is_callback)) {
-		peer_dbg("early exit in a callback\n");
-		return;
-	}
-
-	peer_dbg("before lock\n");
-	ctxlist_lock();
 	peer_dbg("before is tracked\n");
-	if (!__ctxlist_is_tracked(nv_mem_context)) {
+	if (!ctxlist_is_tracked(nv_mem_context)) {
 		peer_err("error, context %px not tracked, ignoring it\n", nv_mem_context);
 		goto out;
 	}
 
+	if (READ_ONCE(nv_mem_context->is_callback)) {
+		peer_dbg("early exit in a callback\n");
+		return;
+	}
+	
 	peer_dbg("nv_mem_context:%px page_table:%px dma_mapping:%px is_callback:%d\n", nv_mem_context, nv_mem_context->page_table, nv_mem_context->dma_mapping, READ_ONCE(nv_mem_context->is_callback));
 
 	ret = nvidia_p2p_put_pages(0, 0, nv_mem_context->page_virt_start,
@@ -644,8 +630,6 @@ out:
 		sg_free_table(sg_head);
 		nv_mem_context->sg_allocated = 0;
 	}
-
-	ctxlist_unlock();
 	return;
 }
 
