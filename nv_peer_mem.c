bb768dab36d8265bbf8952de540b8cb8272f4140
Davide Rossetti <davide.rossetti@gmail.com>
2018-10-31 13:13:45 -0700

    DRV VERSION aligned to deb version ~nvidia1, then bumped to ~nvidia2; minor fix in peer_err; remove unecessary extra setting of peer_memory_client members (reverts a previous change)

--- nv_peer_mem.c
+++ nv_peer_mem.c
@@ -48,10 +48,10 @@
 
 
 #define DRV_NAME	"nv_mem"
-#define DRV_VERSION	"1.0-8war2"
+#define DRV_VERSION	"1.0-8~nvidia2"
 #define DRV_RELDATE	__DATE__
 
-#define peer_err(FMT, ARGS...)  printk(KERN_ERR   DRV_NAME "ERR %s:%d " FMT, __FUNCTION__, __LINE__, ## ARGS)
+#define peer_err(FMT, ARGS...)  printk(KERN_ERR   DRV_NAME " ERR %s:%d " FMT, __FUNCTION__, __LINE__, ## ARGS)
 
 static int enable_info = 1;
 #define peer_info(FMT, ARGS...)                                         \
@@ -759,8 +759,6 @@ static unsigned long nv_mem_get_page_size(void *context)
 
 
 static struct peer_memory_client nv_mem_client = {
-	.name           = DRV_NAME,
-	.version        = DRV_VERSION,
 	.acquire		= nv_mem_acquire,
 	.get_pages	= nv_mem_get_pages,
 	.dma_map	= nv_dma_map,
